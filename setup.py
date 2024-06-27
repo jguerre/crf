11a17451ecc1089faabe2b811d34bdefa9483a80
jguerre <jaimguerrero@gmail.com>
2024-06-27 04:38:53 +0200

    primera version

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="crf",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    description="Librería básica de operaciones aritméticas",
+    author="Jaime Guerrero",
+    author_email="jaimguerrero@gmail.com",
+    url="https://github.com/jguerre/crf",
+)
